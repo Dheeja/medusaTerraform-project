b303b6d40848e30dac8f1c68ff7360c1db21870b
Dheeja <144778266+Dheeja@users.noreply.github.com>
2025-04-08 05:34:52 +0530

    Update provider main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
-variable "aws_region" {
-  default = "ap-south-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0" # Or latest stable version
+    }
+  }
 }
 
 # VPC Configuration
